192da958690cd1a091f3773387af53a316c4cb71
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2020-01-12 15:57:20 +0530

    testkey

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 ssh-keyscan -H $IP >> ~/.ssh/known_hosts
-ls
-
+# Copy generated war files to the server
 scp api/target/api#github-leaderboad#v1.war $USER_NAME@$IP:$DEPLOY_PATH
 scp ui/target/github-leaderboard.war $USER_NAME@$IP:$DEPLOY_PATH
