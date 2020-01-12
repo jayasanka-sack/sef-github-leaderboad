eefe8a87be59cd2276b613ce56db37a612e3956a
Jayasanka Weerasinghe <jayasanka.sack@gmail.com>
2020-01-12 15:50:53 +0530

    testkey

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ set -e
 ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 ls
 
-scp api/target/api.github-leaderboad.v1.war $USER_NAME@$IP:$DEPLOY_PATH
+scp api/target/api#github-leaderboad#v1.war $USER_NAME@$IP:$DEPLOY_PATH
 scp ui/target/github-leaderboard.war $USER_NAME@$IP:$DEPLOY_PATH
